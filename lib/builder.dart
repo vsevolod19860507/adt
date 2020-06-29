373c76727f327776950883a7b2e90f19eae2a712
Vsevolod <vr.19860507@gmail.com>
2020-06-29 18:39:43 +0300

    fixes

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,16 +1,17 @@
 import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
+import 'package:dart_style/dart_style.dart';
 
 import 'code_generator.dart';
 
 Builder adtGeneratorBuilder(BuilderOptions options) => SharedPartBuilder(
       [ADTGenerator()],
       'generator_app',
-      formatOutput: (x) => ignoreForFile + x,
+      formatOutput: (output) => DartFormatter().format(ignoreForFile + output),
     );
 
 const ignoreForFile = '// ignore_for_file: '
     'unnecessary_brace_in_string_interps, '
     'unnecessary_parenthesis, '
     'unused_element, '
-    '\n\n\n\n';
+    '\n\n';
